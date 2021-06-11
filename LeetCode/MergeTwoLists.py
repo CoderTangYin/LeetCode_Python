20abb0a96b7a62abfba110a7730842126a06950b
TangYin <>
2021-06-11 22:39:15 +0800

    no message

--- LeetCode/MergeTwoLists.py
+++ LeetCode/MergeTwoLists.py
@@ -3,6 +3,32 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
+'''
+合并2个有序链表
+ 输入：1->2->4, 1->3->4
+ 输出：1->1->2->3->4->4
+'''
 class Solution:
+	'''
+	迭代解法
+	'''
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    	if l1 == None: return l2
+    	if l2 == None: return l1
+    	# 使用虚拟头节点把整个链表串联起来
+        dummyHead = ListNode(-1)
+        tail = dummyHead
+        while l1 != None && l2 != None:
+        	if l1.val < l2.val:
+        		tail = tail.next = l1
+        		l1 = l1.next
+    		else:
+    			tail = tail.next = l2
+    			l2 = l2.next
+        	pass
+        if l1 == None: tail.next = l2
+    	elif l2 == None: tail.next = l1
+        	pass
     	pass
