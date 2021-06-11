6214042bbb8379dc22efe3c401001bdb8cf18f07
TangYin <>
2021-06-11 22:06:06 +0800

    no message

---
+++ LeetCode/EvalRPN.py
@@ -0,0 +1,34 @@
+
+'''
+示例 1：
+
+输入：tokens = ["2","1","+","3","*"]
+输出：9
+解释：该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+示例 2：
+
+输入：tokens = ["4","13","5","/","+"]
+输出：6
+解释：该算式转化为常见的中缀算术表达式为：(4 + (13 / 5)) = 6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/evaluate-reverse-polish-notation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+    	stack = []
+    	for val in tokens:
+    		if val == '+':
+    			pass
+    		elif val == '-':
+    			pass
+    		elif val == '*'：
+    			pass
+    	    elif val == '/':
+    	    	pass
+    	    else:
+    	    	stack.append(int(i))
+    	return sum(stack)    					
+    	pass
